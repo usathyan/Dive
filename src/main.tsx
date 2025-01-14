354f7deba20e70bf1862758bcc8a405db48483e2
john <john@funmula.com>
2025-01-14 17:27:29 +0800

    feat: init electron

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import './i18n'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import "./i18n"
 
-createRoot(document.getElementById('root')!).render(
+if (window.ipcRenderer) {
+  const originalFetch = window.fetch
+  window.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
+    if (typeof input !== "string" || (typeof input === "string" && !input.startsWith("/api"))) {
+      return originalFetch(input, init)
+    }
+
+    return originalFetch(`http://localhost:${await window.ipcRenderer.port()}${input}`, init)
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
