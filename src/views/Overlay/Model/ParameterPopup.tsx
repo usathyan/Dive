9ad9b626c1d11cefb502d84bc7373076747db4e6
Johnny <johnaa0219@gmail.com>
2025-04-08 15:03:47 +0800

    adjust parameter setting UI

--- src/views/Overlay/Model/ParameterPopup.tsx
+++ src/views/Overlay/Model/ParameterPopup.tsx
@@ -118,12 +118,12 @@ const ParameterPopup = ({
             <div className="parameters-grid">
                 <InfoTooltip
                   maxWidth={270}
-                  side="left"
+                  side="bottom"
                   content={t("setup.topPDescription")}
                 >
                 <div className="parameter-label">
                   <div>TOP-P</div>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 22" width="15" height="15">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 22" width="18" height="18">
                     <g clipPath="url(#ic_information_svg__a)">
                       <circle cx="11.5" cy="11" r="10.25" stroke="currentColor" strokeWidth="1.5"></circle>
                       <path fill="currentColor" d="M9.928 13.596h3.181c-.126-2.062 2.516-2.63 2.516-5.173 0-2.01-1.6-3.677-4.223-3.608-2.229.051-4.08 1.288-4.026 3.9h2.714c0-.824.593-1.168 1.222-1.185.593 0 1.258.326 1.222.962-.144 1.942-2.911 2.389-2.606 5.104Zm1.582 3.591c.988 0 1.779-.618 1.779-1.563 0-.963-.791-1.581-1.78-1.581-.97 0-1.76.618-1.76 1.58 0 .946.79 1.565 1.76 1.565Z"></path>
@@ -148,12 +148,12 @@ const ParameterPopup = ({
             <div className="parameters-grid">
               <InfoTooltip
                 maxWidth={270}
-                side="left"
+                side="bottom"
                 content={t("setup.temperatureDescription")}
               >
                 <div className="parameter-label">
                   <div>Temperature</div>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 22" width="15" height="15">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 22" width="18" height="18">
                     <g clipPath="url(#ic_information_svg__a)">
                       <circle cx="11.5" cy="11" r="10.25" stroke="currentColor" strokeWidth="1.5"></circle>
                       <path fill="currentColor" d="M9.928 13.596h3.181c-.126-2.062 2.516-2.63 2.516-5.173 0-2.01-1.6-3.677-4.223-3.608-2.229.051-4.08 1.288-4.026 3.9h2.714c0-.824.593-1.168 1.222-1.185.593 0 1.258.326 1.222.962-.144 1.942-2.911 2.389-2.606 5.104Zm1.582 3.591c.988 0 1.779-.618 1.779-1.563 0-.963-.791-1.581-1.78-1.581-.97 0-1.76.618-1.76 1.58 0 .946.79 1.565 1.76 1.565Z"></path>
@@ -177,7 +177,7 @@ const ParameterPopup = ({
             </div>
           </div>
         </div>
-        <div className="models-parameter">
+        <div className="models-parameter instructions">
           <label>{t("modelConfig.customInstructions")}</label>
           <div className="instructions-content">
             <WrappedTextarea
